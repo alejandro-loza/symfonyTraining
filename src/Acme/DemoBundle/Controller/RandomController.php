8aedb5fd8a2a546cf009eae0ecc8423ea23be2a9
pinky <orochi_alex@hotmail.com>
2015-05-13 12:47:36 -0500

    random number added

---
+++ src/Acme/DemoBundle/Controller/RandomController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Acme\DemoBundle\Form\ContactType;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+
+class RandomController extends Controller
+{
+    public function indexAction($limit)
+    {
+        return new Response(
+            '<html><body>Number: '.rand(1, $limit).'</body></html>'
+        );
+    }
+}
